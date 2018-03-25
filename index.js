842afd891a063e2fb6f2262f64a4a5d9929e2afa
Christina Nguyen <cnguyen2@sapient.com>
2018-03-25 19:54:26 -0400

    added endpoints to the server

---
+++ index.js
@@ -0,0 +1,40 @@
+// first commit
+// var http = require('http');
+
+// http.createServer(function(req, res) {
+//     res.writeHead(200, {
+//         'Content-Type': 'text/plain'
+//     });
+//     res.end('Hello world\n');
+// }).listen(3000, '127.0.0.1');
+
+// console.log('Server running at http://127.0.01:3000/');
+
+// second commit
+// var express = require('express');
+// var app = express();
+
+// app.get('/', function (req, res) {
+//     res.send('Hello World!');
+//     // res.json({greeting: 'hi world'});
+// });
+
+// var server = app.listen(3000, function () {
+//     console.log('Server running at http://127.0.0.1:3000/');
+// });
+
+// third commit
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var cats = require('./cats.js')(app); // route definitions being put into this file
+
+var server = app.listen(3000, function () {
+    console.log('Server running at http://127.0.0.1:3000/');
+});
