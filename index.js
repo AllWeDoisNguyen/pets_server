18554d8200a12de4d9701c0771adeb9183c03064
Christina Nguyen <cnguyen2@sapient.com>
2018-04-09 12:35:01 -0400

    mongo successfully added to the app

--- index.js
+++ index.js
@@ -24,16 +24,35 @@
 // });
 
 // third commit
+// var express = require('express');
+// var app = express();
+// var bodyParser = require('body-parser');
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+
+// var cats = require('./cats.js')(app); // route definitions being put into this file
+
+// var server = app.listen(3000, function () {
+//     console.log('Server running at http://127.0.0.1:3000/');
+// });
+
+// adding mongo to the App
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-var cats = require('./cats.js')(app); // route definitions being put into this file
+var catRoutes = require('./cat_routes.js')(app);
 
 var server = app.listen(3000, function () {
     console.log('Server running at http://127.0.0.1:3000/');
